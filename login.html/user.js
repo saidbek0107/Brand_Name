926423b0c46889836a58d31a688e49300b54ef50
saidbek_0107 <saidbekochilov86@gmail.com>
2023-10-30 22:40:37 +0900

    first commit

---
+++ login.html/user.js
@@ -0,0 +1,9 @@
+// const btn = document.getElementById('btn');
+
+// btn.addEventListener('click', (e) => {
+//     e.preventDefault()
+
+//     alert('Hisobingizdan barcha pullar yechib olindi.')
+//     alert('0.00 UZS.')
+// })
+
---
+++ script.js
@@ -0,0 +1,48 @@
+
+
+
+const btn1 = document.getElementById('btn1')
+
+btn1.addEventListener('click', (e) => {
+
+    e.preventDefault()
+    alert("404 error. Page Not Found 🚫")
+})
+
+
+const btn2 = document.getElementById('btn2')
+
+btn2.addEventListener('click', (e) => {
+
+    e.preventDefault()
+    alert("404 error. Page Not Found 🚫")
+})
+
+
+const btn3 = document.getElementById('btn3')
+
+btn3.addEventListener('click', (e) => {
+
+    e.preventDefault()
+    alert("404 error. Page Not Found 🚫")
+})
+
+
+const btn4 = document.getElementById('btn4')
+
+btn4.addEventListener('click', (e) => {
+
+    e.preventDefault()
+    alert("404 error. Page Not Found 🚫")
+})
+
+
+const btn5 = document.getElementById('btn5')
+
+btn5.addEventListener('click', (e) => {
+
+    e.preventDefault()
+    alert("404 error. Page Not Found 🚫")
+})
+
+
